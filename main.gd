197bbbd157f2e5558a467e027089f3987d0f0ae0
Pavel Pavlov <kx355@mail.ru>
2025-02-03 23:31:56 +0300

    end of part 4

--- main.gd
+++ main.gd
@@ -33,6 +33,8 @@ func new_game():
 	$Player.show()
 	$GameTimer.start()
 	spawn_coins()
+	$HUD.update_score(score)
+	$HUD.update_timer(time_left)
 
 
 func spawn_coins():
@@ -41,3 +43,31 @@ func spawn_coins():
 		add_child(c)  # add the new coin node to the root node
 		c.screensize = screensize  # share gotten screensize with coin node
 		c.position = Vector2(randi_range(0, screensize.x), randi_range(0, screensize.y))
+
+
+func _on_game_timer_timeout() -> void:
+	time_left -= 1
+	$HUD.update_time(time_left)
+	if time_left < 0:
+		game_over()
+
+
+func _on_player_hurt() -> void:
+	game_over()
+
+
+func _on_player_pickup() -> void:
+	score += 1
+	$HUD.update_score(score)
+
+
+func game_over():
+	playing = false
+	$GameTimer.stop()
+	get_tree().call_group("coins", "queue_free")
+	$HUD.show_game_over()
+	$Player.die()
+
+
+func _on_hud_start_game() -> void:
+	new_game()
